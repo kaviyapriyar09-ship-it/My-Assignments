af5467f16fd0143e3545e0564f978803de35d518
kaviyapriyar09-ship-it <kaviyapriya.r09@gmail.com>
2025-12-17 00:53:14 +0530

    Add files via upload
    Assignment1 Datatype

---
+++ datatypes.js
@@ -0,0 +1,12 @@
+var testername ="Kaviya Priya"
+var organizationname="Jio platforms"
+var contactnumber =9677295929
+var isautomatentester= true
+var playwrightexperience
+var data=null
+console.log("Testername is :"+ testername+" "+typeof(testername))
+console.log("my organization name is :"+ organizationname+" "+typeof(organizationname))
+console.log("my contact number is :"+ contactnumber+" "+typeof(contactnumber))
+console.log(typeof(playwrightexperience))
+console.log("yes i am an automation tester"+ isautomatentester+" "+typeof(isautomatentester)) 
+console.log(typeof(data))
