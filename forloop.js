053a453cdeb3df8182b65e1231816a8d3e9c81fa
kaviyapriyar09-ship-it <kaviyapriya.r09@gmail.com>
2025-12-18 11:36:49 +0530

    Add files via upload
    Day 3 Assignments switch and forloop

---
+++ forloop.js
@@ -0,0 +1,10 @@
+let value=20
+{
+    for(let i=0;i<=value;i++)
+    {
+        if(i%2==1)
+        {
+            console.log( i + " it is a odd number")
+        }
+    }
+}
---
+++ switch.js
@@ -0,0 +1,17 @@
+let day=4
+switch(day){
+    case 1 :
+        console.log("happy sunday")
+        break;
+     case 2:
+        console.log("happy monday")
+        break;
+    case 3:
+        console.log("happy tuesday")    
+        break;
+    case 4:
+        console.log("happy wednesday")    
+        break;
+    default:
+        console.log("Bad Day")
+}
